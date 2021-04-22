406011915da799a54534b22c34fdd2dc7f7d81b0
Ana <atripunoska@gmail.com>
2021-04-22 21:57:01 +0200

    Add Conditinal return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+// import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
@@ -14,21 +15,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filterdExpenses.length > 0) {
-    expensesContent =
-      filterdExpenses.length > 0 &&
-      filterdExpenses.map((filteredItem) => (
-        <ExpenseItem
-          key={filteredItem.id}
-          title={filteredItem.title}
-          amount={filteredItem.amount}
-          date={filteredItem.date}
-        />
-      ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -36,7 +22,7 @@ const Expenses = (props) => {
           selected={selectedYear}
           changeYear={onChangeYearHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filterdExpenses} />
       </Card>
     </div>
   );
